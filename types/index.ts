ed402a2eb0dc2fa92ab611ca9d8ee84311741784
Cal Crosby <calcruiseby@gmail.com>
2024-04-28 16:42:33 -0500

    removed unecessary types

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,7 @@
-import { UUID } from "crypto";
-
 export interface SoundCronJob {
   serverId: string;
   name: string;
-  cron: string;
-  timezone: string;
   audio: string;
   mute: boolean;
   excludeChannels: string[];
-}
-
-export interface SoundCronJobEstablished {
-  key: string;
-  workerId: UUID;
-}
-
-export interface SoundCronHeartbeat {
-  workerId: UUID;
 }
