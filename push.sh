f0d997cac84229e598d75e39c2c58d2de75333f7
Cal Crosby <calcruiseby@gmail.com>
2024-05-11 01:12:24 -0500

    added labels for setup images

--- push.sh
+++ push.sh
@@ -5,14 +5,16 @@
 # When you bump the version of an image, do it by changing the value here.
 
 export CAMPA_VERSION=1.0.0
-
 export CHIMER_VERSION=1.0.0
+export WAREHOUSE_VERSION=1.0.0
 
 export FLYWAY_VERSION=3.0.0
 
 export MINIO_SETUP_VERSION=1.0.0
-
 export MARIADB_SETUP_VERSION=1.0.0
+
+export BUILD_TIME=$(date --iso-8601=seconds)
+export GIT_HASH=$(git rev-parse HEAD)
 
 docker compose build --quiet
 docker compose push --quiet
