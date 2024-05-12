90a8f80c70fa3bbb5466696b2fff70b9d5cfa3c3
Cal Crosby <calcruiseby@gmail.com>
2024-05-11 23:13:03 -0500

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 
 source versions.sh
 
-docker compose --env-file .env.prod up --detach
+docker compose --env-file .env.prod \
+    --file docker-compose.yml \
+    --file docker-compose.prod.override.yml \
+    up --detach
